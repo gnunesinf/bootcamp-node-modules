9db0b09f78ac73e86645ee065ab50c822bffb4b2
Guilherme Nunes <gnunesinf@gmail.com>
2020-01-25 16:55:56 -0300

    dev: finished module, added middlewares and finalized crud

--- module01/index.js
+++ module01/index.js
@@ -11,17 +11,35 @@ server.use((req, res, next) => {
   return next();
 });
 
+function checkUserNameExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required"});
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  
+  req.user = user;
+
+  if(!user) {
+    return res.status(400).json({ error: "User does not exists" });
+  }
+
+  return next();
+}
+
 server.get("/users", (req, res) => {
   return res.json(users);
 });
 
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params;
-
-  return res.json(users[index]);
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserNameExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -29,7 +47,7 @@ server.post("/users", (req, res) => {
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserInArray, checkUserNameExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -38,7 +56,7 @@ server.put("/users/:index", (req, res) => {
   return res.json(users);
 });
 
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
